962bfc78df81c36c383a3aa3b2a09aeec5982e86
Sabin Chapagain <csabinji@gmail.com>
2022-10-17 09:44:18 +0545

    changes in env.js

--- config/env.js
+++ config/env.js
@@ -1,17 +1,21 @@
 const env = process.env;
 
 module.exports = {
+    APP_ENV: env['APP_ENV'],
+    API_URL: env['API_URL'],
+    API_HOST: env['API_HOST'],
     APP_DEBUG: env['APP_DEBUG'],
     ACCESS_TOKEN_LIFETIME: env['ACCESS_TOKEN_LIFETIME'],
-    CODE: env['CODE'],
+
     CLIENT_ID: env['CLIENT_ID'],
     CLIENT_SECRET: env['CLIENT_SECRET'],
-    GET_ACCESS_TOKEN_URL: env['GET_ACCESS_TOKEN_URL'],
-    GRANT_TYPE: env['GRANT_TYPE'],
+
     MAIL_SERVICE: env['MAIL_SERVICE'],
     MAIL_ACCOUNT: env['MAIL_ACCOUNT'],
     MONGO_DB_URL: env['MONGO_DB_URL'],
+
     PORT: env['PORT'],
+
     REDIRECT_URL: env['REDIRECT_URL'],
     REFRESH_TOKEN: env['REFRESH_TOKEN'],
 }
